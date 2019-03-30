22da6587961ae512e07f1c9251c3a683804bd007
shadandeajian <201750080329@stu.csust.edu.cn>
2019-03-30 13:25:59 +0000

    welcome honeli

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">杜朝</div>
+                <div class="title">赵弘历</div>
             </div>
         </div>
     </body>
